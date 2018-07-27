e1af0fdd11a0c45b7f52373e4f4c5c2f580b8bf2
Александр Кармаев <alkarmaev@outlook.com>
2018-07-27 20:59:05 +0300

    Бесконечный цикл

---
+++ ConsoleApplication1/Defs.h
@@ -0,0 +1,7 @@
+//Defs.h: List of definitions for Node class
+
+#pragma once
+
+// Streams should be used for printout info. Stream may be redirected in Microsoft CppUnit
+#define PRINT cout << __FILE__ << ":" << __LINE__ << "->[" << __FUNCTION__ << "]  "
+#define PRINT_ERROR cout << "ERROR###"<< __FILE__ << ":" << __LINE__ << "->[" << __FUNCTION__ << "]  "
